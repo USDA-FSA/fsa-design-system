41fdd1610b220ee2e3123b04e648aff39f744986
Francis Rupert <francisrupert@gmail.com>
2016-07-18 13:59:09 -0500

    playing around with sticky jump menu

--- js/components/playbook.jump.js
+++ js/components/playbook.jump.js
@@ -1,8 +1,33 @@
-
 var $ = require('jquery');
+var Sticky = require('../vendor/jquery.sticky');
 
-$('.pb-jump')
-  .addClass('pb-jump--fixed')
-;
+// $('.pb-jump').sticky({
+//   topSpacing: 0,
+//   className: 'pb-jump--stuck',
+//   wrapperClassName: 'pb-jump-wrapper'
+// });
+//
+// $('.pb-jump').on('sticky-start', function() {
+//   console.log("Stuck");
+// });
+//
+// $('.pb-jump').on('sticky-end', function() {
+//   console.log("UNStuck");
+// });
+
+var $jumpNav = $('.pb-jump');
+
+$(window).on('scroll', function(){
+
+  var heightToShift = $('.pb-jump').height();
+
+  if( $(this).scrollTop() > heightToShift ) {
+    $jumpNav.addClass('pb-jump--stuck');
+  } else {
+    $jumpNav.removeClass('pb-jump--stuck');
+  }
+
+});
+
 
 module.exports = Jump;
