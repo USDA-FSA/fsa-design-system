a7835b8b5b6231050b785588b4ab05568d91b8bd
Francis Rupert <francisrupert@gmail.com>
2016-07-19 11:21:16 -0500

    hooking up sticky jump js

--- js/components/playbook.jump.js
+++ js/components/playbook.jump.js
@@ -1,33 +1,42 @@
 var $ = require('jquery');
 var Sticky = require('../vendor/jquery.sticky');
 
-// $('.pb-jump').sticky({
-//   topSpacing: 0,
-//   className: 'pb-jump--stuck',
-//   wrapperClassName: 'pb-jump-wrapper'
-// });
+if (window.matchMedia("(min-width: 672px)").matches) {
+
+  // If browser doesn't support matchMedia(), we don't care if menu is sticky
+
+  $('.pb-jump').sticky({
+    topSpacing: 40,
+    className: 'pb-jump--stuck',
+    wrapperClassName: 'pb-jump-wrapper'
+  });
+
+  $('.pb-jump').on('sticky-start', function() {
+    console.log("Stuck");
+  });
+
+  $('.pb-jump').on('sticky-end', function() {
+    console.log("UNStuck");
+  });
+
+}
+
+// var $jumpNav = $('.pb-jump');
+
+// $(window).on('scroll', function(){
 //
-// $('.pb-jump').on('sticky-start', function() {
-//   console.log("Stuck");
-// });
+//   var offset = $jumpNav.offset();
+//   var heightToShift = offset.top;
+//   // var heightToShift = 20;
 //
-// $('.pb-jump').on('sticky-end', function() {
-//   console.log("UNStuck");
+//   if( $(this).scrollTop() > heightToShift ) {
+//     $jumpNav.addClass('pb-jump--stuck');
+//   } else {
+//     $jumpNav.removeClass('pb-jump--stuck');
+//   }
+//
+//   console.log('$(this).scrollTop() is ' + $(this).scrollTop() + ' and heightToShift is ' + heightToShift);
+//
 // });
-
-var $jumpNav = $('.pb-jump');
-
-$(window).on('scroll', function(){
-
-  var heightToShift = $('.pb-jump').height();
-
-  if( $(this).scrollTop() > heightToShift ) {
-    $jumpNav.addClass('pb-jump--stuck');
-  } else {
-    $jumpNav.removeClass('pb-jump--stuck');
-  }
-
-});
-
 
 module.exports = Jump;
