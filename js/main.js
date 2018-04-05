9f84afa232c455ce4e2257387f9fed0b099173ce
Francis Rupert <francisrupert@gmail.com>
2018-04-05 14:10:32 -0500

    amends #166

--- js/main.js
+++ js/main.js
@@ -9,4 +9,4 @@ var Growl = require('./components/fsa-growl');
 var Modal = require('./components/fsa-modal');
 var SelectMulti = require('./components/fsa-select-multi');
 var SteppedControl = require('./components/fsa-stepped-control');
-// var ColorDocs = require('./components/docs-color');
+var ColorDocs = require('./components/docs-color');
