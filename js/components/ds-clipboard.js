96ea510eddf43f5872071a3f1ac06db502080e57
Francis Rupert <francisrupert@gmail.com>
2019-02-26 13:38:30 -0600

    Confirmation message for Copy Code [amends #52]

--- js/components/ds-clipboard.js
+++ js/components/ds-clipboard.js
@@ -13,33 +13,39 @@ global.jQuery = require('jquery');
 var $ = global.jQuery;
 window.$ = $;
 
-$(document).ready(function(){
-  $('#ds-doc-code-toggle').prop('checked', false);
-});
-
 var ClipboardJS = require('../vendor/clipboard.js');
 
-$('pre.highlight')
-  .append('<div class="ds-clipboard"><button class="fsa-btn fsa-btn--secondary fsa-btn--small ds-clipboard__btn" title="Copy code to clipboard" data-behavior="copy-code">Copy</button></div>')
-;
+if (ClipboardJS.isSupported()) {
 
-var clipboardardee = new ClipboardJS('[data-behavior~="copy-code"]', {
-  target: function(trigger) {
-    return trigger.parentElement.previousElementSibling;
-  }
-});
+  $(document).ready(function(){
+    $('#ds-doc-code-toggle').prop('checked', false);
+  });
 
+  $('pre.highlight')
+    .append('<div class="ds-clipboard"><button class="fsa-btn fsa-btn--secondary fsa-btn--small ds-clipboard__btn" title="Copy code to clipboard" data-behavior="copy-code">Copy</button></div>')
+  ;
 
-clipboardardee.on('success', function(e) {
+  var clipboardardee = new ClipboardJS('[data-behavior~="copy-code"]', {
+    target: function(trigger) {
+      return trigger.parentElement.previousElementSibling;
+    }
+  });
 
-  console.info('Action:', e.action);
-  console.info('Text:', e.text);
-  console.info('Trigger:', e.trigger);
+  clipboardardee.on('success', function(e) {
 
-  e.clearSelection();
-  e.trigger.blur();
+    console.info('Action:', e.action);
+    console.info('Text:', e.text);
+    console.info('Trigger:', e.trigger);
 
-});
+    e.clearSelection();
+    e.trigger.blur();
 
+    // e.trigger.appendChild('<span>Copied to Clipboard!</span>');
+    e.trigger.insertAdjacentHTML('afterend', '<span class="ds-clipboard__success" role="alert">Copied to Clipboard</span>');
+
+  });
+} else {
+  $('#ds-doc-code-toggle').remove();
+}
 
 console.log('Clipboard Loaded, its JS is NOT to be used for Production, demo purposes only');
