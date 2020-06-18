9ff02a98aeaccfe2cf0d6d2eacb7753bf6f05398
Francis Rupert <francisrupert@gmail.com>
2020-06-18 11:29:57 -0500

    Default Spacing Toggle to on [amends #347]

--- js/components/ds-view-spacing.js
+++ js/components/ds-view-spacing.js
@@ -5,6 +5,6 @@ viewSpacing.init({
   toggleId: 'spacing-toggle-id',
   toggleHolderId: 'spacing-toggle-holder-id',
   holderClassPrefix: 'ds-docs-holder', // 'ds-docs-holder__on' or 'ds-docs-holder__off'
-  isOnInit: false,
+  isOnInit: true,
   useStorage: false
 })
